255119870dac9489cdff3f6a9cfa9c3bfb9a719f
Pavan Kumar Pulivarthi <pavan.kumar.pulivarthi@ibm.com>
2021-07-01 23:26:00 +0530

    create merchant changes

--- tap4food-admin-api/src/main/java/com/endeavour/tap4food/app/controller/AdminController.java
+++ tap4food-admin-api/src/main/java/com/endeavour/tap4food/app/controller/AdminController.java
@@ -1,14 +1,46 @@
 package com.endeavour.tap4food.app.controller;
 
+import java.time.LocalDateTime;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.endeavour.tap4food.app.model.Merchant;
+import com.endeavour.tap4food.app.response.dto.ResponseHolder;
+import com.endeavour.tap4food.app.service.AdminService;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
+
+	@Autowired
+	AdminService adminService;
 	
+	@RequestMapping(value = "/create-merchant", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?>  createMerchant(@Valid @RequestBody Merchant merchant){
+		merchant.setCreatedBy("Admin");
+		boolean createMerchantResFlag = adminService.createMerchant(merchant);
+		ResponseEntity response = null;
+		
+		if(createMerchantResFlag){
+			response =ResponseEntity.ok( ResponseHolder.builder()
+					.status("success")
+					.timestamp(String.valueOf(LocalDateTime.now()))
+					.data("Merchant details saved successfully")
+					.build());
+		}else {
+			response =  ResponseEntity.badRequest().body("Error occurred");
+		}
+		
+		return response;
+		
+	}
 }
--- tap4food-admin-api/src/main/java/com/endeavour/tap4food/app/service/AdminService.java
+++ tap4food-admin-api/src/main/java/com/endeavour/tap4food/app/service/AdminService.java
@@ -1,8 +1,64 @@
 package com.endeavour.tap4food.app.service;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.endeavour.tap4food.app.repository.AdminRepository;
+import com.endeavour.tap4food.app.util.EmailTemplateConstants;
+import com.endeavour.tap4food.app.model.Merchant;
 @Service
 public class AdminService {
 
+	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@Autowired
+	private CommonService commonService;
+	
+	public boolean createMerchant(Merchant merchant) {
+		boolean flag = false;
+		flag = adminRepository.createMerchant(merchant);
+		
+		Optional<Merchant> merchantOptionalObject = adminRepository.findByMerchantByPhoneNumber(merchant.getPhoneNumber());
+		
+		if(merchantOptionalObject.isPresent()) {
+			
+			merchant = merchantOptionalObject.get();
+			
+			if(Objects.isNull(merchant.getUniqueNumber())) {
+				
+				Long uniqNumber = this.getUniqueNumber();
+				
+				merchant.setUniqueNumber(uniqNumber);
+				
+				adminRepository.updateUniqueNumber(merchant);
+				
+				System.out.println("Unique number is updated forthe merchant...");
+				
+				String merchantEmail = merchant.getEmail();
+				
+				String createPasswordLink = "https://qa.d2sid2ekjjxq24.amplifyapp.com/merchant/createPassword?uniqueNumber=" + uniqNumber;
+				
+				String message = commonService.getCreatePasswordHtmlContent().replaceAll(EmailTemplateConstants.CREATE_NEW_PASSWORD_LINK, createPasswordLink)
+						.replaceAll(EmailTemplateConstants.UNIQUE_NUMBER, String.valueOf(uniqNumber));
+				
+				String subject = "Tap4Food registration successfull";
+				
+				commonService.sendEmail(merchantEmail, message, subject);
+			}
+		}
+
+		return flag;
+	}
+	
+	private Long getUniqueNumber() {
+
+		Long uniqNumber = adminRepository.getRecentUniqueNumber();
+
+		return uniqNumber;
+	}
 }
