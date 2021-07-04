1dd8009709d64663daa8d388055a0b4820673acc
TARUN KUMAR <tarunmadasu255@gmail.com>
2021-07-05 01:43:28 +0530

    added upload pic endpoint -lakshmi

---
+++ tap4food-core/src/main/java/com/endeavour/tap4food/app/util/ImageConstants.java
@@ -0,0 +1,10 @@
+package com.endeavour.tap4food.app.util;
+
+public class ImageConstants {
+	
+	public static final String IMAGE_JPEG = "jpeg";
+	public static final String IMAGE_PNG = "png";
+	public static final String IMAGE_JPG = "jpg";
+	
+
+}
--- tap4food-merchant-api/src/main/java/com/endeavour/tap4food/app/controller/MerchantController.java
+++ tap4food-merchant-api/src/main/java/com/endeavour/tap4food/app/controller/MerchantController.java
@@ -1,6 +1,7 @@
 package com.endeavour.tap4food.app.controller;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -18,12 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.endeavour.tap4food.app.model.MenuCategory;
 import com.endeavour.tap4food.app.model.MenuSubCategory;
 import com.endeavour.tap4food.app.model.Merchant;
 import com.endeavour.tap4food.app.response.dto.ResponseHolder;
 import com.endeavour.tap4food.app.service.MerchantService;
+import com.endeavour.tap4food.app.util.ImageConstants;
+/*import org.apache.http.entity.ContentType;*/
 
 @RestController
 @RequestMapping("/api/merchant")
@@ -33,29 +37,25 @@ public class MerchantController {
 	MerchantService merchantService;
 
 	@RequestMapping(value = "/update-status", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseHolder> merchantStatusUpdate(@RequestParam Long uniqueNumber, @RequestParam String status) {
+	public ResponseEntity<ResponseHolder> merchantStatusUpdate(@RequestParam Long uniqueNumber,
+			@RequestParam String status) {
 
 		ResponseEntity<ResponseHolder> responseEntity = null;
-		
+
 		Merchant merchant = merchantService.merchantStatusUpdate(uniqueNumber, status);
 
 		if (!Objects.isNull(merchant)) {
-			
-			ResponseHolder response = ResponseHolder.builder()
-					.status("success")
-					.timestamp(String.valueOf(LocalDateTime.now()))
-					.data(merchant)
-					.build();
-			
+
+			ResponseHolder response = ResponseHolder.builder().status("success")
+					.timestamp(String.valueOf(LocalDateTime.now())).data(merchant).build();
+
 			responseEntity = ResponseEntity.ok(response);
 		} else {
-			
-			ResponseHolder response = ResponseHolder.builder()
-					.status("error")
+
+			ResponseHolder response = ResponseHolder.builder().status("error")
 					.timestamp(String.valueOf(LocalDateTime.now()))
-					.data("No merchanat found. hence the status update not happened.")
-					.build();
-			
+					.data("No merchanat found. hence the status update not happened.").build();
+
 			responseEntity = ResponseEntity.badRequest().body(response);
 		}
 
@@ -85,7 +85,7 @@ public class MerchantController {
 
 	public ResponseEntity<?> updateMerchant(@Valid @RequestBody Merchant merchant) {
 		ResponseEntity response = null;
-		
+
 		Optional<Merchant> merchantRes = merchantService.updateMerchant(merchant);
 		if (merchantRes.isPresent()) {
 			response = ResponseEntity.ok(merchantRes);
@@ -95,57 +95,75 @@ public class MerchantController {
 
 		return response;
 	}
-	
+
 	@RequestMapping(path = "/add-category", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseHolder> createMenuCategory(@Valid @RequestBody MenuCategory menuCategory){
-			
+	public ResponseEntity<ResponseHolder> createMenuCategory(@Valid @RequestBody MenuCategory menuCategory) {
+
 		merchantService.createMenuCategory(menuCategory);
-			
-			ResponseHolder response = ResponseHolder.builder()
-					.status("success")
-					.timestamp(String.valueOf(LocalDateTime.now()))
-					.data(menuCategory)
-					.build();
-			
-			return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
-		
-	} 
-	
-	@RequestMapping(path = "/add-subcategory", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ResponseHolder> createMenuSubCategory(@Valid @RequestBody MenuSubCategory menuSubCategories){
-			
-		merchantService.createMenuSubCategory(menuSubCategories);
-			
-			ResponseHolder response = ResponseHolder.builder()
-					.status("success")
-					.timestamp(String.valueOf(LocalDateTime.now()))
-					.data(menuSubCategories)
-					.build();
-			
-			return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
-	} 
-	
-	@RequestMapping(value = "/fetch-categories", method = RequestMethod.GET)
-	public ResponseEntity<ResponseHolder> getAllCategories(){
-		List<MenuCategory> categoryName = merchantService.getAllCategories();
-		ResponseHolder response = ResponseHolder.builder()
-				.status("Done")
-				.timestamp(String.valueOf(LocalDateTime.now()))
-				.data(categoryName)
-				.build();
+
+		ResponseHolder response = ResponseHolder.builder().status("success")
+				.timestamp(String.valueOf(LocalDateTime.now())).data(menuCategory).build();
+
 		return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
-		
+
 	}
-	
-	@RequestMapping(value = "/{category-id}/fetch-sub-categories", method = RequestMethod.GET)
-	public ResponseEntity<ResponseHolder> getAllSubCategories(@Valid @PathVariable("category-id") String id){
-		Set<MenuSubCategory> subCategories = merchantService.getAllSubCategories(id);
-		ResponseHolder response = ResponseHolder.builder()
-				.status("Done")
-				.timestamp(String.valueOf(LocalDateTime.now()))
-				.data(subCategories)
-				.build();
+
+	@RequestMapping(path = "/add-subcategory", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseHolder> createMenuSubCategory(@Valid @RequestBody MenuSubCategory menuSubCategories) {
+
+		merchantService.createMenuSubCategory(menuSubCategories);
+
+		ResponseHolder response = ResponseHolder.builder().status("success")
+				.timestamp(String.valueOf(LocalDateTime.now())).data(menuSubCategories).build();
+
 		return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
-		
+	}
+
+	@RequestMapping(value = "/fetch-categories", method = RequestMethod.GET)
+	public ResponseEntity<ResponseHolder> getAllCategories() {
+		List<MenuCategory> categoryName = merchantService.getAllCategories();
+		ResponseHolder response = ResponseHolder.builder().status("Done").timestamp(String.valueOf(LocalDateTime.now()))
+				.data(categoryName).build();
+		return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/{category-id}/fetch-sub-categories", method = RequestMethod.GET)
+	public ResponseEntity<ResponseHolder> getAllSubCategories(@Valid @PathVariable("category-id") String id) {
+		Set<MenuSubCategory> subCategories = merchantService.getAllSubCategories(id);
+		ResponseHolder response = ResponseHolder.builder().status("Done").timestamp(String.valueOf(LocalDateTime.now()))
+				.data(subCategories).build();
+		return new ResponseEntity<ResponseHolder>(response, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/{merchant-id}/upload-pic", method = RequestMethod.POST)
+	public ResponseEntity<ResponseHolder> uploadProdilePic(@Valid @PathVariable("merchant-id") String id,
+			@RequestParam(value = "pic", required = false) MultipartFile pic, String type) {
+
+		ResponseEntity<ResponseHolder> response = null;
+		Optional<Merchant> merchantResponse = null;
+
+		System.out.println(pic.getSize());
+		String picType = pic.getOriginalFilename().split("\\.")[1];
+		if (!Arrays.asList(ImageConstants.IMAGE_JPEG, ImageConstants.IMAGE_PNG, ImageConstants.IMAGE_JPG)
+				.contains(picType)) {
+			throw new IllegalStateException("File must be an Image");
+		} else {
+			merchantResponse = merchantService.uploadProfilePic(id, pic, type);
+		}
+
+		if (merchantResponse.isPresent()) {
+
+			response = ResponseEntity.ok(ResponseHolder.builder().status(type + " succesfully uploaded")
+					.timestamp(String.valueOf(LocalDateTime.now())).data(merchantResponse).build());
+		} else {
+			response = ResponseEntity.badRequest()
+					.body(ResponseHolder.builder().status("Error occurred while uploading " + type)
+							.timestamp(String.valueOf(LocalDateTime.now())).data(merchantResponse).build());
+
+		}
+
+		return response;
 	}
 }
